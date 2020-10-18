392b97d75337ae5722ed113290367f9cd9fb4b16
Bastianu <sb_bastianu@orange.fr>
2020-10-18 12:20:58 +0200

    update achat items

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ var myBuildings_Elem = document.getElementById("myBuildings");
 var myRewards_Elem = document.getElementById("myRewards");
 
 //** vars utilisateur */
-var eggs = 0; 
+var eggs = 1000; 
 var clics = 0; 
+var eggsOnclick = 1;
 var secondPassed  = 0; 
 var myBuildings = []; 
 var myBoosts = [];
@@ -20,12 +21,14 @@ var data = [];
 //** vars jeu */
 var timer;
 var eggsThisSecond;
+var currentMultiplier = 1;
 
 
 if(navigator.serviceWorker) {
     navigator.serviceWorker
         .register('sw.js')
 }
+
 
 function initGame(){
     
@@ -34,6 +37,69 @@ function initGame(){
     initClicker();
     initGameTimer();
 }
+
+            //////////////////////////////////
+            // ************FRONT************//
+            //////////////////////////////////
+
+function initBuildings(){ // *** A REFAIRE ***
+    var result = "<ul class=\"list-group\">";
+    for(var i =0; i<data["buildings"].length;i++){
+        result += "<li class=\"list-group-item\"><div>"+ data["buildings"][i]["nom"] +"</div> <div>( "+data["buildings"][i]["production"]+ " <img src='assets/egg.png' width=15> par secondes )</div> <div class=\"btn-group\"><button id=\"buyer_" + data['buildings'][i]['id'] + "\" type=\"button\" class=\"btn btn-outline-info btn-sm\" id='"+data['buildings'][i]['id']+ "' onclick='buyItem("+i+","+data["buildings"][i]["prix"]+", 1)'>"+data["buildings"][i]["prix"]+ " <img src='assets/egg.png' width=15></button></div></li><br>";
+        prices.push(data["buildings"][i]["prix"]);
+    }
+    result += "</ul>";
+    buildings_Elem.innerHTML = result;
+}
+
+function updateRewards(){
+    text = "";
+    for(var i=0; i<myRewards.length ; i++){
+        text += '<div class="myTooltip">'+ myRewards[i][0] +'<span class="tooltiptext">'+myRewards[i][1]+'</span> </div><br>';     
+    }
+    myRewards_Elem.innerHTML = text;
+}
+
+function changeMultiplier(){
+    if(currentMultiplier == 1) currentMultiplier = 10;
+    else if(currentMultiplier == 10) currentMultiplier = 100;
+    else if(currentMultiplier == 100) currentMultiplier = 1;
+    applyMultiplier();
+}
+
+function applyMultiplier(){
+        document.getElementById("multiplier").innerText = "x"+currentMultiplier;      
+        for(var i =0; i<data["buildings"].length;i++){
+            totalcost =0;
+            for (var j=0;j<currentMultiplier;j++) totalcost += Math.round(prices[i]*Math.pow(1.05, myBuildings[i]+j));
+            document.getElementById("buyer_" + (i+1) ).innerHTML =  totalcost  + " <img src='assets/egg.png' width=15>";
+            document.getElementById("buyer_" + (i+1)).setAttribute("onclick", "buyItem("+i+","+totalcost+","+currentMultiplier+")");    
+        }
+}
+
+function updateEggs(){
+    egg_Elem.innerHTML = Math.round(eggs);
+}
+
+function updateEggsPerSec(){
+    eggps_Elem.innerHTML = eggsThisSecond+" oeufs par secondes";
+}
+
+function updateMyBuildings(){
+    text = "";
+    for(var i=0; i<myBuildings.length ; i++){
+        if(myBuildings[i]!=0){
+            text += data["buildings"][i]["nom"] + " x " + myBuildings[i] + " ( "+ (data["buildings"][i]["production"]* myBuildings[i])+"/s ) <br>";
+        }
+        
+    }
+    myBuildings_Elem.innerHTML = text;
+}
+
+
+            //////////////////////////////////
+            // *************BACK************//
+            //////////////////////////////////
 
 function getPlayerData(){
     //get vars utilisateur from cache ou from bdd
@@ -62,46 +128,15 @@ function initClicker(){
     clicker.onclick = clicked;
 }
 
-function initBuildings(){ // *** A REFAIRE ***
-    var result = "<ul class=\"list-group\">";
-    for(var i =0; i<data["buildings"].length;i++){
-        result += "<li class=\"list-group-item\"><div>"+ data["buildings"][i]["nom"] +"</div> <div>( "+data["buildings"][i]["production"]+ " <img src='assets/egg.png' width=15> par secondes )</div> <div class=\"btn-group\"><button id=\"buyer_" + data['buildings'][i]['id'] + "\" type=\"button\" class=\"btn btn-outline-info btn-sm\" id='"+data['buildings'][i]['id']+ "' onclick='buyOne("+i+","+data["buildings"][i]["prix"]*Math.pow(1.05,myBuildings[i])+")'>"+data["buildings"][i]["prix"]+ " <img src='assets/egg.png' width=15></button></div></li><br>";
-        prices.push(data["buildings"][i]["prix"]);
-    }
-    result += "</ul>";
-    buildings_Elem.innerHTML = result;
+function clicked(){
+    addClic();
+    eggs += eggsOnClick;
+    updateEggs();
 }
+
 
 function initGameTimer(){
     timer = setInterval(game_timer, 1000);
-}
-
-function changeMultiplier(){
-    var multiplier = document.getElementById("multiplier").innerText;
-    if(multiplier === "x1"){
-        document.getElementById("multiplier").innerText = "x10";
-        
-        for(var i =0; i<data["buildings"].length;i++){
-            document.getElementById("buyer_" + (i+1) ).innerHTML = prices[i] * 10  + "<img src='assets/egg.png' width=15>"
-            document.getElementById("buyer_" + (i+1)).setAttribute("onclick", "buyTen("+i+","+data["buildings"][i]["prix"]*Math.pow(1.05,myBuildings[i])+")")
-        }
-    }
-    else if(multiplier === "x10"){
-        document.getElementById("multiplier").innerText = "x100";
-
-        for(var i =0; i<data["buildings"].length;i++){
-            document.getElementById("buyer_" + (i+1) ).innerHTML = prices[i] * 100  + "<img src='assets/egg.png' width=15>"
-            document.getElementById("buyer_" + (i+1)).setAttribute("onclick", "buyHundred("+i+","+data["buildings"][i]["prix"]*Math.pow(1.05,myBuildings[i])+")")
-        }
-    }
-    else if(multiplier === "x100"){
-        document.getElementById("multiplier").innerText = "x1";
-
-        for(var i =0; i<data["buildings"].length;i++){
-            document.getElementById("buyer_" + (i+1) ).innerHTML = prices[i]  + "<img src='assets/egg.png' width=15>"
-            document.getElementById("buyer_" + (i+1)).setAttribute("onclick", "buyOne("+i+","+data["buildings"][i]["prix"]*Math.pow(1.05,myBuildings[i])+")")
-        }
-    }
 }
 
 function game_timer(){
@@ -118,46 +153,13 @@ function game_timer(){
 }
 
 
-function clicked(){
-    addClic();
-    eggs +=1;
-    updateEggs();
-}
-
-function updateEggs(){
-    egg_Elem.innerHTML = Math.round(eggs);
-}
-
-function updateEggsPerSec(){
-    eggps_Elem.innerHTML = eggsThisSecond+" oeufs par secondes";
-}
-
-function updateBuildings(){
-    text = "";
-    for(var i=0; i<myBuildings.length ; i++){
-        if(myBuildings[i]!=0){
-            text += data["buildings"][i]["nom"] + " x " + myBuildings[i] + " ( "+ (data["buildings"][i]["production"]* myBuildings[i])+"/s ) <br>";
-        }
-        
-    }
-    myBuildings_Elem.innerHTML = text;
-}
-
-function updateRewards(){
-    text = "";
-    for(var i=0; i<myRewards.length ; i++){
-        text += '<div class="myTooltip">'+ myRewards[i][0] +'<span class="tooltiptext">'+myRewards[i][1]+'</span> </div><br>';     
-    }
-    myRewards_Elem.innerHTML = text;
-}
-
-function buyOne(i, prix){
+function buyItem(i, prix, nb){
     if(eggs - prix >= 0){
-        myBuildings[i] ++; 
+        myBuildings[i] += nb; 
         eggs -= prix;
         updateEggs();
-        updateBuildings();
-        increaseBuildingCost(i);
+        updateMyBuildings();
+        applyMultiplier(); //increaseBuildingCost(i, nb);
         console.log(myBuildings); 
     }
     else {
@@ -165,46 +167,6 @@ function buyOne(i, prix){
     }   
 }
 
-function buyTen(i, prix){
-    if(eggs - (prix*10) >= 0){
-        myBuildings[i] += 10; 
-        eggs -= prix * 10;
-        updateEggs();
-        updateBuildings();
-        increaseBuildingCostTen(i);
-        console.log(myBuildings); 
-    }
-    else {
-        console.log("pas assez d'oeufs")
-    }   
-}
-
-function buyHundred(i, prix){
-    if(eggs - (prix*100) >= 0){
-        myBuildings[i] += 10; 
-        eggs -= prix * 100;
-        updateEggs();
-        updateBuildings();
-        increaseBuildingCostHundred(i);
-        console.log(myBuildings); 
-    }
-    else {
-        console.log("pas assez d'oeufs")
-    }   
-}
-
-function buyMax(i){
-    var count = 0;
-    while(eggs - prix > 0){
-        myBuildings[i] ++; 
-        eggs -= prix;
-        updateEggs();
-        updateBuildings();
-        increaseBuildingCost(i);
-        count++;
-    }
-    console.log(count+ " achetés");
-}
 
 function stop(){
     clearInterval(timer);   
@@ -261,32 +223,17 @@ function addSecond(){
     });
 }
 
-function increaseBuildingCost(i){
-    var initialcost = data["buildings"][i]["prix"];
+
+/*
+function increaseBuildingCost(i, nbBought){
+    /*var initialcost = data["buildings"][i]["prix"];
     var nbOfThisBuilding = myBuildings[i];
-    var newCost = Math.round(initialcost*Math.pow(1.05, nbOfThisBuilding-1));
+    var newCost = Math.round(initialcost*Math.pow(1.05, nbOfThisBuilding-1+nbBought));
     document.getElementById("buyer_" + (i+1)).innerHTML = newCost+ " <img src='assets/egg.png' width=15>";
-    document.getElementById("buyer_" + (i+1)).setAttribute('onclick','buyOne('+i+','+newCost+')');
+    document.getElementById("buyer_" + (i+1)).setAttribute('onclick','buyItem('+i+','+newCost+',1)');
     prices[i] = newCost;
 }
-
-function increaseBuildingCostTen(i){
-    var initialcost = data["buildings"][i]["prix"];
-    var nbOfThisBuilding = myBuildings[i];
-    var newCost = Math.round(initialcost*Math.pow(1.05, nbOfThisBuilding-1));
-    document.getElementById("buyer_" + (i+1)).innerHTML = newCost * 10 + " <img src='assets/egg.png' width=15>";
-    document.getElementById("buyer_" + (i+1)).setAttribute('onclick','buyTen('+i+','+newCost+')');
-    prices[i] = newCost;
-}
-
-function increaseBuildingCostHundred(i){
-    var initialcost = data["buildings"][i]["prix"];
-    var nbOfThisBuilding = myBuildings[i];
-    var newCost = Math.round(initialcost*Math.pow(1.05, nbOfThisBuilding-1));
-    document.getElementById("buyer_" + (i+1)).innerHTML = newCost * 100 + " <img src='assets/egg.png' width=15>";
-    document.getElementById("buyer_" + (i+1)).setAttribute('onclick','buyTen('+i+','+newCost+')');
-    prices[i] = newCost;
-}
+*/
 
 // **** lecture content.json
 /*function readTextFile(file, callback) {
@@ -308,3 +255,36 @@ readTextFile("content.json", function(text){
 // ****
 
 
+/*
+function changeMultiplier2(){
+    var multiplier = document.getElementById("multiplier").innerText;
+    if(multiplier === "x1"){
+        document.getElementById("multiplier").innerText = "x10";
+        
+        for(var i =0; i<data["buildings"].length;i++){
+            totalcost =0;
+            for (var j=0;j<10;j++) totalcost += Math.round(prices[i]*Math.pow(1.05, myBuildings[i]+j));
+            document.getElementById("buyer_" + (i+1) ).innerHTML =  totalcost  + "<img src='assets/egg.png' width=15>";
+            document.getElementById("buyer_" + (i+1)).setAttribute("onclick", "buyItem("+i+","+totalcost+", 10)");    
+        }
+    }
+    else if(multiplier === "x10"){
+        document.getElementById("multiplier").innerText = "x100";
+
+        for(var i =0; i<data["buildings"].length;i++){
+            totalcost =0;
+            for (var j=0;j<100;j++) totalcost += Math.round(prices[i]*Math.pow(1.05, myBuildings[i]+j));
+            document.getElementById("buyer_" + (i+1) ).innerHTML = totalcost  + "<img src='assets/egg.png' width=15>"
+            document.getElementById("buyer_" + (i+1)).setAttribute("onclick", "buyItem("+i+","+totalcost+"), 100")
+        }
+    }
+    else if(multiplier === "x100"){
+        document.getElementById("multiplier").innerText = "x1";
+
+        for(var i =0; i<data["buildings"].length;i++){
+            document.getElementById("buyer_" + (i+1) ).innerHTML = prices[i]  + "<img src='assets/egg.png' width=15>"
+            document.getElementById("buyer_" + (i+1)).setAttribute("onclick", "buyItem("+i+","+prices[i]+",1)")
+        }
+    }
+}
+*/
